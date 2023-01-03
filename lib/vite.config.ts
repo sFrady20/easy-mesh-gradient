9169c2bfb53111a443fedad28761a6aad64331f8
zabuter <sfrady20@gmail.com>
2023-01-03 14:39:37 -0500

    feat: added gh pages CD

--- lib/vite.config.ts
+++ lib/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [dts()],
   build: {
     lib: {
       name: "easy-mesh-gradient",
