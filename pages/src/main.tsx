002ad608adb1d59314e55147853f619feff9790c
zabuter <sfrady20@gmail.com>
2023-01-03 21:57:49 -0500

    feat: added fancy sharing info

--- pages/src/main.tsx
+++ pages/src/main.tsx
@@ -1,11 +1,15 @@
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ReactGA from "react-ga";
 import "virtual:windi.css";
 import "./styles.css";
 
+ReactGA.initialize("G-85RZBGK3H5");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <>
+  <StrictMode>
     <App />
-  </>
+  </StrictMode>
 );
