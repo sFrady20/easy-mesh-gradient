00219b6f14235016f483beb80d05357803a5bb2e
zabuter <sfrady20@gmail.com>
2023-01-03 22:18:13 -0500

    feat: gtag

--- pages/src/main.tsx
+++ pages/src/main.tsx
@@ -1,12 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ReactGA from "react-ga";
 import "virtual:windi.css";
 import "./styles.css";
-
-ReactGA.initialize("G-85RZBGK3H5");
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
